835ccaebbd134b70f3e43bb9a89f11b6f58208ed
hanangjie <hanangjie@gmail.com>
2016-06-27 17:30:53 +0800

    debu

--- getData/medicalDetailPlanB.js
+++ getData/medicalDetailPlanB.js
@@ -4,6 +4,7 @@
  */
 var mysql = require('mysql');
 const http = require('http');
+var request1 = require('request');
 
 var TEST_DATABASE = 'data';
 var TEST_TABLE = 'medicalname';
@@ -76,26 +77,10 @@ function c(r,r1){
         if(!e.html){
             console.log("id:"+e.medicalId+"name:"+r1[e.medicalId-2].name+"html:"+r1[e.medicalId-2].intro);
         (function(i){
-            var postData = JSON.stringify({
-                'msg' : 'Hello World!'
-            });
-            //console.log("%d\t%s\t%s", results[i].id, results[i].name, results[i].intro);
-            var options = {
-                hostname: 'baike.baidu.com',
-                path: r1[i].intro,
-                method: 'get',
-                headers: {
-                    'Content-Type':"application/x-www-form-urlencoded"
-                }
-            };
-            var req = http.request(options, (res) => {
-                var html=""
-                res.setEncoding('utf8');
-                res.on('data', (chunk) => {
-                    //console.log(`BODY: ${chunk}`);
-                    html+=chunk
-                });
-                res.on('end', () => {
+            request1('http://baike.baidu.com'+r1[i].intro, function (error, response, body) {
+                var html
+                if (!error && response.statusCode == 200) {
+                    //console.log(body) // 打印google首页
                     var client2 = mysql.createConnection({
                         host: 'localhost',
                         user: 'root',
@@ -103,11 +88,10 @@ function c(r,r1){
                         database:'data',
                         port: 3306
                     });
-                    console.log(html);
-                    html=html.replace(/\"/g,"\\\"")
+                    html=body.replace(/\"/g,"\\\"")
                     try{
                         if(!html){
-                            console.log(html);
+                            console.log("error"+html);
                             return
                         }
                         sql=`insert into medicalHtml (html,medicalId) values ("${html}","${r1[i].id}")`
@@ -129,14 +113,74 @@ function c(r,r1){
                         console.log(e)
                         console.log("error:"+r1[i])
                     }
-                    /* fs.writeFile('medicalnameTest.html', `${html}`, (err) => {
+                }
+            })
+           /* console.log(i)
+            var postData = JSON.stringify({
+                'msg' : 'Hello World!'
+            });
+            //console.log("%d\t%s\t%s", results[i].id, results[i].name, results[i].intro);
+            var options = {
+                hostname: 'baike.baidu.com',
+                path: r1[i].intro,
+                method: 'get',
+                headers: {
+                    'Content-Encoding':'deflate',
+                    'Content-Type': 'text/html',
+                    "Transfer-Encoding":'chunked',
+                    'Vary':'accept-encoding',
+                    'Tracecode':'10293055880874983946062717'
+                }
+            };
+            var req = http.request(options, (res) => {
+                var html=""
+                res.setEncoding('utf8');
+                res.on('data', (chunk) => {
+                    console.log(`BODY: ${chunk}`);
+                    html+=chunk
+                });
+                res.on('end', () => {
+                    console.log('end');
+                    var client2 = mysql.createConnection({
+                        host: 'localhost',
+                        user: 'root',
+                        password: '',
+                        database:'data',
+                        port: 3306
+                    });
+                    html=html.replace(/\"/g,"\\\"")
+                    try{
+                        if(!html){
+                            console.log("error"+html);
+                            return
+                        }
+                        sql=`insert into medicalHtml (html,medicalId) values ("${html}","${r1[i].id}")`
+                        client2.connect();
+                        client2.query(
+                            sql,
+                            function selectCb(err, results, fields) {
+                                if (err) {
+                                    throw err;
+                                }
+                                if(results)
+                                {
+                                    console.log("success")
+                                }
+                                client2.end();
+                            }
+                        );
+                    }catch(e){
+                        console.log(e)
+                        console.log("error:"+r1[i])
+                    }
+                    /!* fs.writeFile('medicalnameTest.html', `${html}`, (err) => {
                      if (err) throw err;
                      console.log('It\'s saved!');
-                     });*/
+                     });*!/
                 })
             });
 
-            /*req.on('error', (e) => {
+            req.on('error', (e) => {
                 console.log(`problem with request: ${e.message}`);
             });
 
